87bb7315cfc14880cd21b5a000b532715f026724
Francesco <francesco@hv-a.com>
2012-01-18 12:31:05 +0100

    limiting max value for grid rows and columns using GL_MAX_EVAL_ORDER, sees we are going to resign to refactor things using a quad or triangle based mesh

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -244,8 +244,10 @@ void testApp::setup()
         gui.addToggle("surface deform.", quads[i].bDeform);
         gui.addToggle("use bezier", quads[i].bBezier);
         gui.addToggle("use grid", quads[i].bGrid);
-        gui.addSlider("grid rows", quads[i].gridRows, 2, 12);
-        gui.addSlider("grid columns", quads[i].gridColumns, 2, 16);
+        GLint value ;
+        ::glGetIntegerv(GL_MAX_EVAL_ORDER, &value) ;
+        gui.addSlider("grid rows", quads[i].gridRows, 2, value-1);
+        gui.addSlider("grid columns", quads[i].gridColumns, 2, value-1);
         gui.addButton("spherize light", bQuadBezierSpherize);
         gui.addButton("spherize strong", bQuadBezierSpherizeStrong);
         gui.addButton("reset bezier warp", bQuadBezierReset);
