e473931a831e722d0cdf0327d2836998aef30813
francesco fantoni <francesco@hv-a.com>
2014-04-10 09:06:08 +0200

    forcing use of Glut as window manager, was not able to make it work properly with default GLFW

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include "testApp.h"
 #include "ofMain.h"
-
+#include "ofAppGlutWindow.h"
 
 //========================================================================
 int main( )
 {
-    ofSetupOpenGL(800,600, OF_WINDOW);			// <-------- setup the GL context
+	
+	
+	
+    ofAppGlutWindow* window = new ofAppGlutWindow();
+    //window->setOpenGLVersion(4, 3);    // should be default value, but if i use this, ofSphere() gone
+    // window->setOpenGLVersion(3, 2);  // same with (4, 3)
+    // window->setOpenGLVersion(2, 1);  // glfwCreateWindow failed
+
+
+	
+	
+	
+    ofSetupOpenGL(window, 800,600, OF_WINDOW);			// <-------- setup the GL context
 
 
 
