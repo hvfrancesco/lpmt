ed0b518e22318ed960a26d7324f6b032a2f8a5ca
francesco fantoni <francesco@hv-a.com>
2014-04-10 09:09:09 +0200

    cleaning a bit

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,12 @@ int main( )
 	
 	
     ofAppGlutWindow* window = new ofAppGlutWindow();
-    //window->setOpenGLVersion(4, 3);    // should be default value, but if i use this, ofSphere() gone
-    // window->setOpenGLVersion(3, 2);  // same with (4, 3)
-    // window->setOpenGLVersion(2, 1);  // glfwCreateWindow failed
+    //window->setOpenGLVersion(4, 3);    // should be default value
+    // window->setOpenGLVersion(3, 2);  // 
+    // window->setOpenGLVersion(2, 1);  // 
 
-
-	
-	
 	
     ofSetupOpenGL(window, 800,600, OF_WINDOW);			// <-------- setup the GL context
-
-
 
     // this kicks off the running of my app
     // can be OF_WINDOW or OF_FULLSCREEN
